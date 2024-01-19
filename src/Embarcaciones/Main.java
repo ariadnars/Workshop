2c6e29cffc31697876774e4db316ccf236bd3a98
Ariadna <ariadnaelena2@gmail.com>
2024-01-18 23:33:47 -0500

    2 commit

--- src/Embarcaciones/Main.java
+++ src/Embarcaciones/Main.java
@@ -5,21 +5,30 @@ public class Main {
 
         Capitan ariadna = new Capitan("Ariadna","Rodriguez","123456");
         Capitan jhon = new Capitan("Jhon", "Hernandez","12345678");
-        Yate yate1 = new Yate(ariadna,30000,20000,2010, 15,5);
+        Yate yate1 = new Yate(ariadna,40000,20000,2010, 15,5);
         Yate yate2 = new Yate(jhon,30000,20000,2023, 15,14);
-        Velero velero1 = new Velero(ariadna,30000,20000,2010, 15,3);
-        Velero velero2 = new Velero(jhon,30000,20000,2023, 15,5);
-        System.out.println(yate1.comprarYate());
+        Velero velero1 = new Velero(ariadna,50000,20000,2010, 15,3);
+        Velero velero2 = new Velero(jhon,60000,20000,2023, 15,5);
 
-        System.out.println("EL yate del capitan "+ ariadna.getNombre() + yate2.comprarYate());
+        System.out.println("EL yate1 tiene como capitan a: " + yate1.getCapitan().getNombre());
+        System.out.println("EL yate2 tiene como capitan a: " + yate2.getCapitan().getNombre());
 
-        System.out.println(yate1.isLuxury());
+        System.out.println("El yate1 es de lujo: " + yate1.isLuxury());
+        System.out.println("El yate2 es de lujo: " + yate2.isLuxury());
 
-        System.out.println(yate2.isLuxury());
+        System.out.println("El velero1 es grande: " + velero1.isBig());
+        System.out.println("El velero2 es grande: " + velero2.isBig());
 
-        System.out.println(velero1.isBig());
+        System.out.println("Se esta tramitando la compra del yate1, esta compra se pudo hacer?: " + yate1.comprarYate());
+        System.out.println("Se esta tramitando la compra del yate1, esta compra se pudo hacer?: " + yate1.comprarYate());
 
-        System.out.println(velero2.isBig());
+        System.out.println("El aquiler del yate1 es: " + yate1.calcularAlquiler());
+        System.out.println("El aquiler del yate2 es: " + yate2.calcularAlquiler());
+
+        System.out.println("El aquiler del velero1 es: " + velero1.calcularAlquiler());
+        System.out.println("El aquiler del velero2 es: " + velero2.calcularAlquiler());
+
+
 
 
 
